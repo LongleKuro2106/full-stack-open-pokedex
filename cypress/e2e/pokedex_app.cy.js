b60e70bd9c484870ea2ccb1f0e7dc784b770e930
Long Le <long.le21062004@gmail.com>
2025-01-17 01:05:31 +0200

    Additional e2e test

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -5,4 +5,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  // New test for navigating to a Pokémon page
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click() // Click on the Ivysaur link
+    cy.contains('chlorophyll') // Check for the presence of the ability
+  })
 })
